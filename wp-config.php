2a9ab8888b4b90fe9dc17cb2207275e6a9f80fc4
SonNH <sonk54a3@gmail.com>
2014-09-05 13:29:31 +0700

    update new server database and config htaccess

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'flagteam');
+define('DB_NAME', 'sql551538');
 
 /** MySQL database username */
-define('DB_USER', 'flagteam');
+define('DB_USER', 'sql551538');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'flagteam123');
+define('DB_PASSWORD', 'eS9*cG4!');
 
 /** MySQL hostname */
-define('DB_HOST', 'db4free.net:3306/flagteam');
+define('DB_HOST', 'sql5.freemysqlhosting.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
